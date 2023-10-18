52be55b0e8d069bf149722ded671aecea43a539e
sourpatchkov <alanosnovnoy@gmail.com>
2023-10-17 20:23:37 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 002</h2>
+      <p>WVU ID: 800398304</p>
+      <p>Hi I am Alan Pachkovskiy</p>
+    </div>
+  );
+}
+
+export default App;
