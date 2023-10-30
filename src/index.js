7df6fcd295777832341a5e378143f44a9eea4320
sourpatchkov <alanosnovnoy@gmail.com>
2023-10-30 15:57:23 -0400

    added nav and cards

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css";
+import Nav from './nav.js';
+import Cards from './cards.js';
+import './cards.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Nav/>
     <App />
+    <Cards/>
   </React.StrictMode>
 );
 
